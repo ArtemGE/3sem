4d2ec636c66924db41871950c4ae8750cfc18bee
Gryazev_Artem_E <fire1112@mail.ru>
2017-10-07 11:21:32 +0300

    The error in 4_exec_dir.c was corrected, comments about it was added

--- task_1/examples/4_exec_dir.c
+++ task_1/examples/4_exec_dir.c
@@ -1,5 +1,5 @@
-/* 1) build and examine this code
-   2) correct the mistake: it is possible to hang on wait. Why and when? (see "man fork")
+/* 1) build and examine this code !@@@! Done !@@@!
+   2) correct the mistake: it is possible to hang on wait. Why and when? (see "man fork") !@@@! Done !@@@!
  */
 
 #include <unistd.h>
@@ -9,10 +9,12 @@
 int main() {
 	pid_t pid;
    	int status, died;
-   	switch(pid = fork()) {
+	switch(pid = fork()) {
    		case 0: 
 			execl("/bin/ls","ls","-l",NULL); // this is the code the child runs 
-   		default: 
+   		case -1:
+			printf("Error: new process wasn't created.");//If OS can't create new process (for ex. there are no possible PID's in it) the parent process will be waited the signal from the child process up to the end of OS working (or, maybe, up to SIGKILL or so on).
+		default: 
 			wait(&status); // this is the code the parent runs
    	}
 	return 0;
